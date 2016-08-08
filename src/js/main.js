19030e3e0085b5bdfcf603659700dbecef52f007
Xaquín G.V <xocasgv@gmail.com>
2016-08-08 10:54:04 +0100

    Took out email share, added source style

--- src/js/main.js
+++ src/js/main.js
@@ -56,14 +56,12 @@ function renderRelated(resp){
 
 function share(network, shareURL, message) {
 
+    var shareWindow;
+
         if (network === 'twitter') {
             shareWindow = twitterBaseUrl + encodeURIComponent(message + ' ') + shareURL;
         } else if (network === 'facebook') {
             shareWindow = facebookBaseUrl + shareURL;
-        } else if (network === 'email') {
-            shareWindow = 'mailto:?subject=' + encodeURIComponent(title) + '&body=' + shareURL;
-        } else if (network === 'google') {
-            shareWindow = googleBaseUrl + shareURL;
         }
 
         window.open(shareWindow, network + 'share', 'width=640,height=320');
